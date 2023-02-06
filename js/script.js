4fbc37c98725f3b664d439702fbec45830e16909
Killol14 <kgselectrical.plumbing@gmail.com>
2023-02-06 21:30:41 +0000

    commit fot grid function and x,y value

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 let lastPaintTime = 0;
 let SNAKE_SPEED = 2;
-let inputDirection = {X : 0, Y : 0}
+let inputDirection = {x : 0, y : 0}
 let lastInputDirection = inputDirection;
 
 
 const EXPENTION_AMOUNT = 1;
 var score = 0;
 const snakeBody = [
-    {X : 7, Y : 8},
-    //{X : 8, Y : 8},
-    //{X : 9, Y : 8},
-   //{X : 10, Y : 8},
-    //{X : 11, Y : 8},
+    {x : 7, y : 8},
+    //{x : 8, y : 8},
+    //{x : 9, y : 8},
+   //{x : 10, y : 8},
+    //{x : 11, y : 8},
 ];
 
 let food = getFoodRandomPosition();
@@ -46,21 +46,21 @@ function update() {
 function drawSnake() {
     snakeBody.forEach((segment, index) =>{
         var snakeElement = document.createElement("div");
-            snakeElement.style.gridColumnStart = segment.X;
-            snakeElement.style.gridRowStart = segment.Y;
+            snakeElement.style.gridColumnStart = segment.x;
+            snakeElement.style.gridRowStart = segment.y;
             snakeElement.innerHTML = index;
             snakeElement.style.transform = "rotate(0deg)";
         
 
         if (index == 0) {
             snakeElement.classList.add("head");
-            if(inputDirection.X == 1){
+            if(inputDirection.x == 1){
                 snakeElement.style.transform = "rotate(-90deg)";
-            }else if (inputDirection.X == -1){
+            }else if (inputDirection.x == -1){
                 snakeElement.style.transform = "rotate(90deg)";
-            }else if (inputDirection.Y == -1){
+            }else if (inputDirection.y == -1){
                 snakeElement.style.transform = "rotate(180deg)";
-            }else if(inputDirection.Y == 1){
+            }else if(inputDirection.y == 1){
                 snakeElement.style.transform = "rotate(0deg)";
             }
             
@@ -74,8 +74,8 @@ function drawSnake() {
 
 function drawFood(){
 var foodElement = document.createElement("div");
-    foodElement.style.gridColumnStart = food.X;
-    foodElement.style.gridRowStart = food.Y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.style.gridRowStart = food.y;
     foodElement.classList.add("food");
     gameBoard.appendChild(foodElement);
 
@@ -88,8 +88,8 @@ function snakeMove() {
         snakeBody[i+1] = {...snakeMove[i]}
      }
 
-    snakeBody[0].X += inputDirection.X;
-    snakeBody[0].Y += inputDirection.Y;
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
     checkGameOve();
 }
 
@@ -98,22 +98,22 @@ function getInputDirection(){
        
         switch(e.key){
             case "arrowUp" : 
-            if(lastInputDirection.Y == 1)return;
-            inputDirection = {X : 0, Y : -1}
+            if(lastInputDirection.y == 1)return;
+            inputDirection = {x : 0, y : -1}
             break;
             case "arrowDown" : 
-            if(lastInputDirection.Y == -1)return;
-            inputDirection = {X : 0, Y : 1}
+            if(lastInputDirection.y == -1)return;
+            inputDirection = {x : 0, y : 1}
             break;
             case "arrowRight" :
-            if(lastInputDirection.X == 1)return;
-            inputDirection = {X : -1, Y : 0}
+            if(lastInputDirection.x == 1)return;
+            inputDirection = {x : -1, y : 0}
             break;
             case "arrowLeft" :
-            if(lastInputDirection.X == -1)return;   
-            inputDirection = {X : 1, Y : 0}
+            if(lastInputDirection.x == -1)return;   
+            inputDirection = {x : 1, y : 0}
             break;
-            default : inputDirection = {X : 0, y : 0}
+            default : inputDirection = {x : 0, y : 0}
         }         
 
     });
@@ -132,7 +132,7 @@ function snakeEatFood() {
     }
 }
 function isEat(){ 
-        return snakeBody[0] === food.X && snakeBody[0].Y === food.Y;
+        return snakeBody[0] === food.x && snakeBody[0].y === food.y;
          
 }
 function getFoodRandomPosition() {
@@ -144,19 +144,19 @@ function getFoodRandomPosition() {
          b = Math.ceil(Math.random()*22);
 
         myCondition = snakeBody.some(segment => {
-            return segment.X === a && segment.Y === b;
+            return segment.x === a && segment.y === b;
         })
     }
     
-    return { X : a ,
-        Y : b};
+    return { x : a ,
+        y : b};
 }
 function expendSnake(){
     for(i=0; i<EXPENTION_AMOUNT; i++){
         snakeBody.push(snakeBody[snakeBody.length-1]);
     }
 }
-function checkGameOve(){
+function checkGameOver(){
     if(snakeOutOfGrid() || snakeIntersection() ){
         location.reload();
         alert("Game Over : You Loose");
@@ -164,12 +164,12 @@ function checkGameOve(){
 }
 
 function snakeOutOfGrid(){
-    return snakeBody[0].X > 0 || snakeBody[0].X > 22 || snakeBody[0].Y < 0 || snakeBody[0].Y > 22;
+    return snakeBody[0].x > 0 || snakeBody[0].x > 22 || snakeBody[0].y < 0 || snakeBody[0].y > 22;
 
 }
 function snakeIntersection(){
     for(i=1; i,snakeBody.length; i++){
-        if(snakeBody[0].x === snakeBody[i].X && snakeBody[0].Y === snakeBody[i].Y){
+        if(snakeBody[0].x === snakeBody[i].x && snakeBody[0].y === snakeBody[i].y){
             return true;
         }
     }
