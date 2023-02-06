8282d16545c96402f08781169b20fdf0c7140670
Killol14 <kgselectrical.plumbing@gmail.com>
2023-02-06 14:00:11 +0000

    commit for js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 let lastPaintTime = 0;
 const SNAKE_SPEED = 1;
 let inputDirection = {X : 0, Y : 0}
+let lastInputDirection = inputDirection;
 
 const snakeBody = [
-    {X : 7, Y : 8}
-]
+    {X : 7, Y : 8},
+    {X : 8, Y : 8},
+    {X : 9, Y : 8},
+    {X : 10, Y : 8},
+    {X : 11, Y : 8},
+];
+
+const gameBoard = document.querySelector(".game-board");
+
 function paint(currentTime){
     let TimeSeconds = (currentTime - lastPaintTime) / 1000;
     requestAnimationFrame(paint);
@@ -16,8 +24,6 @@ function paint(currentTime){
 
 }
 window.requestAnimationFrame(paint);
-
-const gameBoard = document.querySelector(".game-board");
 
 function draw() {
     drawSnake()
@@ -31,12 +37,19 @@ function update() {
 }
 
 function drawSnake() {
-    snakeBody.forEach(segment =>{
+    snakeBody.forEach((segment, index) =>{
         let snakeElement = document.createElement("div");
         snakeElement.style.gridColumnStart = segment.X;
         snakeElement.style.gridRowStart = segment.Y;
+        snakeElement.innerHTML = index;
+        
 
-        snakeElement.classList.add("snake");
+        if (index == 0) {
+            snakeElement.classList.add("head");
+            
+        }else {
+            snakeElement.classList.add("snake");
+        }
 
         gameBoard.appendChild(snakeElement);
     });
@@ -44,6 +57,10 @@ function drawSnake() {
 
 function snakeMove() {
     inputDirection = getInputDirection();
+
+     for(i = snakeBody.length -2; i >= 0; i--){
+        snakeBody[i+1] = {...snakeMove[i]}
+     }
 
     snakeBody[0].X += inputDirection.X;
     snakeBody[0].Y += inputDirection.Y;
@@ -54,17 +71,26 @@ function getInputDirection(){
     window.addEventListener("keydown", e=> {
        
         switch(e.key){
-            case "arrowUp" : inputDirection = {X : 0, Y : -1}
+            case "arrowUp" : 
+            if(lastInputDirection.Y == 1)return;
+            inputDirection = {X : 0, Y : -1}
             break;
-            case "arrowDown" : inputDirection = {X : 0, Y : 1}
+            case "arrowDown" : 
+            if(lastInputDirection.Y == -1)return;
+            inputDirection = {X : 0, Y : 1}
             break;
-            case "arrowRight" : inputDirection = {X : 0, Y : 0}
+            case "arrowRight" :
+            if(lastInputDirection.X == 1)return;
+            inputDirection = {X : -1, Y : 0}
             break;
-            case "arrowLeft" : inputDirection = {X : 0, Y : 0}
+            case "arrowLeft" :
+            if(lastInputDirection.X == -1)return;   
+            inputDirection = {X : 1, Y : 0}
             break;
             default : inputDirection = {X : 0, y : 0}
         }         
 
     })
+    lastInputDirection = inputDirection;
     return inputDirection;
 }
