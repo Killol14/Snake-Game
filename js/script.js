4b5f04f06c373d16c757ea4799a557440e57e72f
Killol14 <kgselectrical.plumbing@gmail.com>
2023-02-11 22:41:53 +0000

    this commit is for bug finding for scripts.js button

--- js/script.js
+++ js/script.js
@@ -1,101 +1,105 @@
+// A very basic snake game code.
 let grid = document.querySelector(".grid");
-let popup = document.querySelector(".popup")
-let playAgain = document.querySelector(".playAgain")
-let scoreDisplay = document.querySelector(".scoreDisplay")
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay");
 let left = document.querySelector(".left");
 let bottom = document.querySelector(".bottom");
 let right = document.querySelector(".right");
-let up = document.querySelector(".up");
-let width = 15;
+let up = document.querySelector(".top");
+let width = 20;
 let currentIndex = 0;
 let appleIndex = 0;
-let currentSnake =[2, 1, 0];
-let score= 0;
+let currentSnake = [2, 1, 0];
 let direction = 1;
-let speed = 0.8;
+let score = 0;
+let speed = 1;
 let intervalTime = 0;
 let interval = 0;
 
-//DOMContentLoad
+window.onload = function () {
+  createBoard();
+  startGame();
+  playAgain.addEventListener("click", replay);
+};
 
-
-document.addEventListener("DOMContentLoaded" , function () {
-    document.addEventListener("keydown", control);
-    createBoard();
-    startGame();
-    playAgain.addEventListener("click", replay);
-});
-
+// Creating CreateBoard function
 function createBoard() {
-    popup.style.display = "none";
-    for(let i = 0; i > 225; i++) {
-        let div = document.createElement("div");
-        grid.appendChild(div);
-
-    }
+  popup.style.display = "none";
+  for (let i = 0; i < 400; i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div);
+  }
 }
-function startGame(){
-    let squares = document.querySelectorAll(".grid div");
+
+// Start of the game
+function startGame() {
+  let squares = document.querySelectorAll(".grid div");
+  randomApple(squares);
+  //random apple
+  direction = 1;
+  scoreDisplay.innerHTML = score;
+  intervalTime = 500;
+  currentSnake = [2, 1, 0];
+  currentIndex = 0;
+  interval = setInterval(moveOutcome, intervalTime);
+}
+
+function moveOutcome() {
+  let squares = document.querySelectorAll(".grid div");
+  if (checkForHits(squares)) {
+    alert("Oops!! You hit something");
+    popup.style.display = "flex";
+    score = 0;
+    return clearInterval(interval);
+  } else {
+    moveSnake(squares);
+  }
+}
+
+function moveSnake(squares) {
+  let tail = currentSnake.pop();
+  squares[tail].classList.remove("snake");
+  currentSnake.unshift(currentSnake[0] + direction);
+  //movement ends here
+  eatApple(squares, tail);
+  squares[currentSnake[0]].classList.add("snake");
+}
+
+function checkForHits(squares) {
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width <= 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// eating the food provided
+function eatApple(squares, tail) {
+  if (squares[currentSnake[0]].classList.contains("apple")) {
+    squares[currentSnake[0]].classList.remove("apple");
+    squares[tail].classList.add("snake");
+    currentSnake.push(tail);
     randomApple(squares);
-    direction = 1;
-    scoreDisplay.innerHTML = score;
-    intervalTime = 1000;
-    currentSnake = [2, 1, 0];
-    currentIndex = 0;
-    currentSnake.forEach((index) => squares[index].classList.add("snake"));
+    score++;
+    scoreDisplay.textContent = score;
+    clearInterval(interval);
+    intervalTime = intervalTime * speed;
     interval = setInterval(moveOutcome, intervalTime);
+  }
 }
 
-function moveOutcome(){
-    let squares = document.querySelectorAll(".grid div")
-    if(checkForHits(squares)){
-        alert("Game Over!");
-        popup.style.display = "flex";
-        return clearInterval(interval);
-    }else{
-        moveSnake(squares);
-    }
-}
-
-function moveSnake(squares){
-    let tail = currentSnake.pop();
-    squares[tail].classList.remove("snake");
-    currentSnake.unshift(currentSnake[0] + direction);
-    eatApple(squares, tail);
-    squares[currentSnake[0]].classList.add("snake");
-}
-function checkForHits(squares){
-    if(
-        (currentSnake[0] + width >= width * width && direction === width)||
-        (currentSnake[0] + width === width -1  && direction === 1)||
-        (currentSnake[0] + width === 0 && direction === -1)||
-        (currentSnake[0] + width <= 0 && direction === -width)||
-        squares[currentSnake[0] + direction].classList.contains("snake")
-    ){
-        return true;
-    }else{
-        return false;
-        }
-}
-
-function eatApple(squares, tail){
-    if(squares[currentSnake[0]].classList.contains("apple")){
-        squares[currentSnake[0]].classList.remove("apple");
-        squares[tail].classList.add("snake");
-        currentSnake.push(tail);
-        randomApple(squares);
-        score++;
-        scoreDisplay.textContent = score;
-        clearInterval(interval);
-        intervalTime = intervalTime * speed;
-        interval = setInterval(moveOutcome, intervalTime);
-    }
-}
-function randomApple(squares){
-    do{
-        appleIndex = Math.floor(Math.random() * squares.length);
-    }while(squares[appleIndex].classList.contains("snake"));
-    squares[appleIndex].classList.add("apple");
+function randomApple(squares) {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length);
+  } while (squares[appleIndex].classList.contains("snake"));
+  squares[appleIndex].classList.add("apple");
 }
 
 //buttons function
@@ -113,19 +117,20 @@ function control(e) {
 
 }
 
-up.addEventListener("click", () => (directon = -width));
-bottom.addEventListener("click", () => (directon = +width));
-left.addEventListener("click", () => (directon = -1));
-right.addEventListener("click", () => (directon = 1));
+up.addEventListener("click", () => (direction = -width));
+bottom.addEventListener("click", () => (direction = +width));
+left.addEventListener("click", () => (direction = -1));
+right.addEventListener("click", () => (direction = 1));
 
 function replay(){
      
-    grid.innerHTML = "";
+    gameBoard.innerHTML = "";
     score = 0;
-    createBoard();
-    startGame();
+    snakeMove();
     popup.style.display ="none";
     
 
 }
+document.onkeyup = (e) => control(e);
+
 
